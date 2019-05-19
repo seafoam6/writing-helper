09001c142023d017026b4d3deb8c4b34a56d41b8
Steve Barman <steve@stevebarman.com>
2019-05-19 14:46:33 -0400

    passable

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App, {arrOfNumbers, setOfThings, stuffToDo, getRandomThing} from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('arrOfNumbers', () => {
+  it('returns an array of numbers', () => {
+    expect(arrOfNumbers(4)).toEqual([1,2,3,4])
+  })
+})
+
+describe('setOfThings', () => {
+  it('returns an array of things', () => {
+    expect(setOfThings(4, "DA DA DA")).toMatchSnapshot()
+  })
+})
+
+describe('stuffToDo', () => {
+  it('returns an array of things', () => {
+    expect(stuffToDo).toMatchSnapshot()
+  })
+})
+
+describe('getRandomThing', () => {
+  it('returns an array of things', () => {
+    const thing = getRandomThing();
+    expect(stuffToDo.indexOf(thing)).not.toEqual(-1)
+  })
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import {Button} from 'grommet';
+import styled from 'styled-components'
+
+export const arrOfNumbers = (count: number): number[] => {
+  let i = 0;
+  const thing = [];
+  while (i < count){
+    i++;
+    thing.push(i)
+  }
+  return thing;
+}
+
+export const setOfThings = (count: number, thingToDo: string): string[] => {
+  return arrOfNumbers(count).map(i => `${thingToDo} ${i}`)
+}
+
+export const stuffToDo = [
+  "read script",
+ ...setOfThings(4, 'read outline'), 
+ ...setOfThings(4, 'write part of'), 
+ "work on character outline"
+]
+
+export const getRandomThing = () => {
+  return stuffToDo[Math.floor(Math.random() * stuffToDo.length)];
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height:100vh
+`
 
 const App: React.FC = () => {
+  const defaultText = 'PUSH ME'
+   const [buttonText, setButtonText] = useState(defaultText);
+
+  const handleClick = () => {
+    const thingToDo =  getRandomThing();
+
+    setButtonText(thingToDo);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Button onClick={handleClick} label={buttonText} />
+      <Button onClick={()=> setButtonText(defaultText)} label='Reset' />
+    </Wrapper>
   );
 }
 
