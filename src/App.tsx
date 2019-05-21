1bba83c085477ba102d9d665546c9ffd9b04eed8
Steve Barman <steve@stevebarman.com>
2019-05-21 06:44:14 -0400

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,32 @@
 import React, { useState } from 'react';
-import {Button} from 'grommet';
-import styled from 'styled-components'
-import {
-stuffToDo, 
-} from './data/stuffToDo'
-import {getRandomThing} from './utils/generic';
+import { Button } from 'grommet';
+import styled from 'styled-components';
+import { stuffToDo } from './data/stuffToDo';
+import { getRandomThing } from './utils/generic';
 
 const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-around;
-  height:100vh;
-`
+  height: 100vh;
+`;
 
 const App: React.FC = () => {
-  const defaultText = 'PUSH ME'
-   const [buttonText, setButtonText] = useState(defaultText);
+  const defaultText = 'PUSH ME';
+  const [buttonText, setButtonText] = useState(defaultText);
 
   const handleClick = () => {
-    const thingToDo =  getRandomThing(stuffToDo);
+    const thingToDo = getRandomThing(stuffToDo);
 
     setButtonText(thingToDo);
-  }
+  };
 
   return (
     <Wrapper>
       <Button onClick={handleClick} label={buttonText} />
-      <Button onClick={()=> setButtonText(defaultText)} label='Reset' />
+      <Button onClick={() => setButtonText(defaultText)} label="Reset" />
     </Wrapper>
   );
-}
+};
 
 export default App;
