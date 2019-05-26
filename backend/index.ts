d8630e05a8fea2f2c4aec212d82a551eb5e0c8e4
Steve Barman <steve@stevebarman.com>
2019-05-26 09:59:46 -0400

    env working

--- backend/index.ts
+++ backend/index.ts
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+require("dotenv").config();
+
+console.log("secrets", process.env.PGUSER, process.env.PGDATABASE);
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
